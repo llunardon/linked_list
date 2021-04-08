f6c1cabf31be08a9918fca0c9d845551a71783bb
llunardon <lorenzo.lunardon.1@gmail.com>
2021-04-08 11:26:04 +0200

    created basic functions, still buggy

---
+++ linked_list.rb
@@ -0,0 +1,130 @@
+require_relative 'node.rb'
+
+class LinkedList
+  attr_reader :size
+
+  def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next_node = @tail
+    @size = 2
+  end
+
+  def append(value)
+    new_tail = Node.new
+    new_tail.value = value
+    @tail.next_node = new_tail
+    @tail = new_tail
+
+    @size += 1
+  end
+
+  def prepend(value)
+    new_head = Node.new
+    new_head.value = value
+    new_head.next_node = @head
+    @head = new_head
+
+    @size += 1
+  end
+
+  #def size
+  #  curr = @head
+  #  i = 1
+
+  #  until curr.next_node == nil do
+  #    curr = curr.next_node
+  #    i += 1
+  #  end
+
+  #  i
+  #end
+
+  def head
+    @head
+  end
+
+  def tail
+    @tail
+  end
+
+  def at(index)
+    if index > size - 1 
+      puts 'Index is too large'
+      return nil
+    end
+
+    curr = @head
+    i = 0
+
+    until i == index do
+      curr = curr.next_node
+      i += 1
+    end
+
+    curr
+  end
+
+  def pop
+    curr = @head
+
+    until curr.next_node == tail do
+      curr = curr.next_node
+    end 
+
+    old_tail = @tail
+
+    @tail = curr
+    @tail.next_node = nil
+
+    old_tail
+  end
+
+  def contains?(value)
+    curr = @head
+    found = false
+
+    until curr.next_node == nil do
+      if curr.value == value
+        found = true
+      end
+
+      curr = curr.next_node
+    end
+
+    found
+  end
+
+  def find(value)
+    curr = @head
+    i = 0
+    ret_value = nil
+
+    until curr.next_node == nil do
+      if (curr.value == value)
+        ret_value = i
+        return ret_value
+      end
+
+      i += 1
+    end
+
+    ret_value
+  end
+
+  def to_s
+    curr = @head
+
+    until curr == @tail do
+      print "(#{curr.value}) -> "
+
+      curr = curr.next_node
+    end
+  end
+end
+
+linked_list = LinkedList.new
+linked_list.prepend(3)
+linked_list.append(2)
+
+linked_list.to_s
